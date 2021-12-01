e34678a04e805815956c57c0f29fd6c3c30457ad
정노현 <53748656+chaeno@users.noreply.github.com>
2021-12-01 22:37:45 +0900

    rust ch12-1

---
+++ rust/minigrep/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("인수를 구문분석하는 동안 오류가 발생했습니다: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("애플리케이션 에러: {}", e);
+        process::exit(1);
+    }
+}
