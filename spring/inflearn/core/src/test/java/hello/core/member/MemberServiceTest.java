25d1a0e57a5b9dca58e81fc1d6fb715e37a0895e
정노현 <53748656+chaeno@users.noreply.github.com>
2022-01-05 07:34:07 +0900

    TIL 21.01.05

--- spring/inflearn/core/src/test/java/hello/core/member/MemberServiceTest.java
+++ spring/inflearn/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -15,7 +15,7 @@ public class MemberServiceTest {
         // when
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
-        
+
         // then
         Assertions.assertThat(member).isEqualTo(findMember);
     }
