128b83a40fe62b3750c972563a84ca7ffe21858f
정노현 <53748656+chaeno@users.noreply.github.com>
2022-02-24 08:53:59 +0900

    22.02.24

---
+++ kotlin/kotlin_in_action/src/ch04/4.2.3_1_ImplementingPropertiesDeclaredInInterfaces.kt
@@ -0,0 +1,24 @@
+package ch04.ex2_3_1_ImplementingPropertiesDeclaredInInterfaces
+
+fun getFacebookName(accountId: Int) = "fb:$accountId"
+
+interface User {
+    val nickname: String
+}
+
+class PrivateUser(override val nickname: String) : User
+
+class SubscribingUser(private val email: String) : User {
+    override val nickname: String
+        get() = email.substringBefore('@')
+}
+
+class FacebookUser(accountId: Int) : User {
+    override val nickname =  getFacebookName(accountId)
+}
+
+fun main(args: Array<String>) {
+    println(PrivateUser("test@kotlinlang.org").nickname)
+    println(SubscribingUser("test@kotlinlang.org").nickname)
+    println(FacebookUser(1234).nickname)
+}
